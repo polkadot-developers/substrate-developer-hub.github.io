c44520bacb2b2b4da563f16d3045b7e5a522f2ac
Joshy Orndorff <admin@joshyorndorff.com>
2019-05-11 12:33:46 +0200

    Remove blog

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -41,7 +41,7 @@ const siteConfig = {
   headerLinks: [
     {page: 'docs', label: 'Docs'},
     {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    //{blog: true, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
