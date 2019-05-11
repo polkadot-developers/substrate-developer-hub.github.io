d016ed29f40ec026ddb85cd1871d1c8126bba00b
Joshy Orndorff <admin@joshyorndorff.com>
2019-05-11 12:29:32 +0200

    Start updating site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,17 +21,18 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'Substrate Developers\' Hub', // Title for your website.
+  tagline: 'Replace this landing page with Eric\'s design',
+
+  url: 'https://paritytech.github.io/', // Your website URL
+  baseUrl: '/substrate-develeoper-hub/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'substrate-developer-hub',
+  organizationName: 'paritytech',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
