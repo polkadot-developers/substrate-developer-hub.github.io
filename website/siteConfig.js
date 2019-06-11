b3fe47405ef1562b93a3ab1676431fe843867953
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-06-11 02:34:07 +0200

    Updated header link to getting-started

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -184,7 +184,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {page: 'docs/quickstart/installing-substrate', label: 'Docs'},
+    {page: 'docs/quickstart/getting-started', label: 'Docs'},
     {page: 'tutorials', label: 'Tutorials'},
     {href: 'https://www.parity.io/blog/', label: 'Blog'},
     {page: 'community/support', label: 'Community'},
