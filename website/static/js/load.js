2b8cadedadad18b84550be88280b5339be1962b1
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-09-17 17:20:14 +0200

    autoformat

--- website/static/js/load.js
+++ website/static/js/load.js
@@ -1,16 +1,19 @@
-var head = document.getElementsByTagName('head')[0];
+var head = document.getElementsByTagName("head")[0];
 
 // Global site tag (gtag.js) - Google Analytics with Klaro script
-var gtagScript = document.createElement('script');
+var gtagScript = document.createElement("script");
 gtagScript.async = true;
-gtagScript.type = 'opt-in';
-gtagScript.setAttribute('data-type', 'application/javascript');
-gtagScript.setAttribute('data-src', 'https://www.googletagmanager.com/gtag/js?id=UA-145158313-2');
-gtagScript.setAttribute('data-name', 'googleAnalytics');
+gtagScript.type = "opt-in";
+gtagScript.setAttribute("data-type", "application/javascript");
+gtagScript.setAttribute(
+  "data-src",
+  "https://www.googletagmanager.com/gtag/js?id=UA-145158313-2"
+);
+gtagScript.setAttribute("data-name", "googleAnalytics");
 head.appendChild(gtagScript);
 
-var dataLayerScript = document.createElement('script');
-dataLayerScript.type = 'opt-in';
+var dataLayerScript = document.createElement("script");
+dataLayerScript.type = "opt-in";
 dataLayerScript.text = `
 window.dataLayer = window.dataLayer || [];
 function gtag(){
@@ -19,6 +22,6 @@ function gtag(){
 gtag('js', new Date());
 gtag('config', 'UA-145158313-2');
 `;
-dataLayerScript.setAttribute('data-type', 'application/javascript');
-dataLayerScript.setAttribute('data-name', 'googleAnalytics');
+dataLayerScript.setAttribute("data-type", "application/javascript");
+dataLayerScript.setAttribute("data-name", "googleAnalytics");
 head.appendChild(dataLayerScript);
