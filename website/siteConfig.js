c6c427e2a761b834ac1e64635a7beb414c65277f
Benjamin Kampmann <ben@gnunicorn.org>
2019-10-01 23:27:57 +0200

    ensure we stretch the image rather than repeat it

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -155,7 +155,7 @@ const siteConfig = {
     intro: "Hacktoberfest is here! ",
     link: "https://substrate.dev/hacktoberfest",
     label: "Hack with us",
-    background: "url(/img/hacktoberfest-blast-bg.png)"
+    background: "url(/img/hacktoberfest-blast-bg.png); background-size: cover;"
   }
 };
 
