33beaa3bfe14c08186f2d0cfc72a36a613d82ff0
Max S <hello@thisisvilla.com>
2019-12-03 17:27:48 +0100

    newsletterpage

---
+++ website/pages/en/newsletter.js
@@ -0,0 +1,20 @@
+const React = require('react')
+
+class Newsletter extends React.Component {
+	render() {
+		return (
+			<section id='newsletter'>
+				<div className='container'>
+					<div class='_form_7' />
+					<script
+						src='https://paritytechnologies.activehosted.com/f/embed.php?id=7'
+						type='text/javascript'
+						charset='utf-8'
+					/>
+				</div>
+			</section>
+		)
+	}
+}
+
+module.exports = Newsletter
