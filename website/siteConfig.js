510faf22847f2df2c3be4e3bf31e8a0e427ecdd4
Jimmy Chu <jimmy@parity.io>
2019-12-14 12:51:12 +0800

    Remove sticky footer (sub0 event) as it is passed. (#369)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -162,13 +162,13 @@ const siteConfig = {
     }
   },
 
-  // customised blast banner on top
-  blast: {
-    img: '/img/sub0-blast.png',
-    link: 'https://sub0.parity.io',
-    background: 'url(/img/bg-sub01.svg); background-size: cover; background-position: right 0px bottom 500px;',
-    fontColor: '#18FFB2'
-  }
+  // Sticky footer config
+  // blast: {
+  //   img: '/img/sub0-blast.png',
+  //   link: 'https://sub0.parity.io',
+  //   background: 'url(/img/bg-sub01.svg); background-size: cover; background-position: right 0px bottom 500px;',
+  //   fontColor: '#18FFB2'
+  // }
 };
 
 module.exports = siteConfig;
