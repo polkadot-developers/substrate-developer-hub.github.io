ffa531ef9ad2d3805dfb0026da133b6ccce7640d
Joshy Orndorff <admin@joshyorndorff.com>
2020-01-30 21:11:12 -0500

    Update videos

--- website/pages/en/seminar.js
+++ website/pages/en/seminar.js
@@ -182,7 +182,29 @@ function Seminar(props) {
 							/>
 							<a
 								className='video-play'
-								href='https://www.youtube.com/watch?v=b3HIXufMHnc&list=PLp0_ueXY_enXRfoaW7sTudeQH10yDvFOS&index=1'
+								href='https://www.youtube.com/watch?v=7qkqEfToH8w&list=PLp0_ueXY_enXRfoaW7sTudeQH10yDvFOS'
+								target='_blank'
+							/>
+						</section>
+						<section className='video-details'>
+							<h4 className='p-color'>Six ways to Launch a Token</h4>
+							<p>
+								We demonstrate several ways to launch tokens with Substrate, explore transaction fees, and take a closer look at the EVM module.
+							</p>
+						</section>
+					</div>
+
+					<div className='col-md-4'>
+						<section className='video-cover video-cover-xs'>
+							<div
+								className='background-image-holder'
+								style={{
+									backgroundImage: `url('/img/seminar-t.jpg' )`
+								}}
+							/>
+							<a
+								className='video-play'
+								href='https://www.youtube.com/watch?v=b3HIXufMHnc&list=PLp0_ueXY_enXRfoaW7sTudeQH10yDvFOS'
 								target='_blank'
 							/>
 						</section>
@@ -204,7 +226,7 @@ function Seminar(props) {
 							/>
 							<a
 								className='video-play'
-								href='https://www.youtube.com/watch?v=abdyjAL1yGA&list=PLp0_ueXY_enXRfoaW7sTudeQH10yDvFOS&index=2'
+								href='https://www.youtube.com/watch?v=abdyjAL1yGA&list=PLp0_ueXY_enXRfoaW7sTudeQH10yDvFOS'
 								target='_blank'
 							/>
 						</section>
@@ -216,30 +238,6 @@ function Seminar(props) {
 						</section>
 					</div>
 
-
-						<div className='col-md-4'>
-							<section className='video-cover video-cover-xs'>
-								<div
-									className='background-image-holder'
-									style={{
-										backgroundImage: `url('/img/seminar-t.jpg' )`
-									}}
-								/>
-								<a
-									className='video-play'
-									href='https://www.youtube.com/watch?v=lKpp6-4OHwA&list=PLp0_ueXY_enXRfoaW7sTudeQH10yDvFOS&index=3'
-									target='_blank'
-								/>
-							</section>
-							<section className='video-details'>
-								<h4 className='p-color'>Offchain Workers</h4>
-								<p>
-									Jimmy Chu joins us to talk about offchain workers and his price-fetching demonstration.
-								</p>
-							</section>
-						</div>
-
-					</div>
 				</div>
 
 				<div
