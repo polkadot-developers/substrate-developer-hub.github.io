53e6317ffff40dc59aa200b41fb1e342ff9bcb43
Joshy Orndorff <admin@joshyorndorff.com>
2020-03-06 08:26:59 -0500

    Post plans for March 10th.

--- website/pages/en/seminar.js
+++ website/pages/en/seminar.js
@@ -104,8 +104,15 @@ function Seminar(props) {
 						<div className='col-md-6 first'>
 							<h4 className='mb-2'>What topics are coming up?</h4>
 							<p>
-								On <strong>March 3rd</strong>, Matej will show a front-end template based on VueJS.
-								We will also cover several miscellaneous topics including Proof of Work.
+								On <strong>March 10th</strong>, we will learn about two Substrate ecosystem projects,{' '}
+								<a href="https://github.com/akropolisio/polkahub-monorepo">PolkaHub</a>, and <a href="https://totemaccounting.com/">Totem Live Accounting</a>.
+							</p>
+							<p>
+								<strong>PolkaHub</strong> is a containerized tool for fast deployment and re-deployment of Substrate-based nodes.
+							</p>
+							<p>
+								<strong>Totem</strong> has been building building p2p accounting for the gig economy on Substrate since 2018.
+								Chris from the Totem team will conduct an intreractive session with anyone who wants to participate, using their live UI and network.
 							</p>
 							<h4 className='mt-4 mb-2'>Can I share a project I've been working on?</h4>
 							<p>
@@ -129,8 +136,11 @@ function Seminar(props) {
 						<div className='col-md-6 second'>
 							<h4 className='mb-2'>How should I prepare for the call?</h4>
 							<p>
-								For the March 3rd call, there are no specific preparation steps. You may find it
-								useful to complete one of our{' '}
+								There are no specific preparation steps. You may find it useful to read the links for{' '}
+								<a href="https://github.com/akropolisio/polkahub-monorepo">PolkaHub</a>{' '}
+								and{' '}
+								<a href="https://totemaccounting.com/">Totem Live Accounting</a>
+								{' '}or complete one of our{' '}
 								<a href='https://substrate.dev/en/tutorials'>
 									Introductory Tutorials
 								</a>{' '}.
