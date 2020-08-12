74afc91f5ad37fe676cbce57e7799f51e5205453
Joshy Orndorff <JoshOrndorff@users.noreply.github.com>
2020-08-12 19:01:06 -0400

    Tutorials Page: no new tabs (#625)
    * prune target _blank
    
    * prune unused component
    
    * Clean unnecessary component nesting
    
    * Start to remove unused recipes component, but then decide to keep it in case we should return to revisit idea.

--- website/pages/en/tutorials.js
+++ website/pages/en/tutorials.js
@@ -35,9 +35,9 @@ class Tutorials extends React.Component {
     const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
     const pageUrl = page => baseUrl + (language ? `${language}/` : "") + page;
 
-    const FeaturedTutorialCards = props =>
+    const TutorialCards = props =>
       props.data.map(tutorial => (
-        <a href={tutorial.href} target="_blank" className="a_wrapper">
+        <a href={tutorial.href} className="a_wrapper">
           <Col xl={3} lg={4} md={6} sm={12} className="mb-5 d-flex align-items-stretch">
             <Card>
               <Card.Img
@@ -104,7 +104,6 @@ class Tutorials extends React.Component {
                   variant="secondary"
                   className="primary-color"
                   href={tutorial.href}
-                  target="_blank"
                 >
                   Try it now!
               </Button>
@@ -114,78 +113,7 @@ class Tutorials extends React.Component {
         </a>
       ));
 
-    const OtherTutorialCards = props =>
-      props.data.map(tutorial => (
-        <a href={tutorial.href} target="_blank" className="a_wrapper">
-          <Col xl={3} lg={4} md={6} sm={12} className="mb-5 d-flex align-items-stretch">
-            <Card>
-              <Card.Body className="d-flex flex-column">
-                <Card.Title>{tutorial.title}</Card.Title>
-                <Card.Text>{tutorial.text}</Card.Text>
-                <div className="mt-auto">
-                  <Badge
-                    variant={
-                      tutorial.difficulty == `hard`
-                        ? `danger`
-                        : tutorial.difficulty == `medium`
-                          ? `warning`
-                          : `success`
-                    }
-                    className="m-1"
-                  >
-                    {tutorial.difficulty.charAt(0).toUpperCase() +
-                      tutorial.difficulty.slice(1)}
-                  </Badge>
-                  <Badge
-                    variant={
-                      tutorial.length > 4
-                        ? `danger`
-                        : tutorial.length > 2
-                          ? `warning`
-                          : `success`
-                    }
-                    className="m-1"
-                  >
-                    {tutorial.length} Hour{tutorial.length > 1 ? `s` : ``}
-                  </Badge>
-                  <Badge
-                    variant={
-                      tutorial.prerequisite == true ? `warning` : `success`
-                    }
-                    className="m-1"
-                  >
-                    {tutorial.prerequisite == true
-                      ? `Prerequisites`
-                      : `No Prerequisites`}
-                  </Badge>
-                  <Badge
-                    variant={
-                      tutorial.version <= 1
-                        ? `danger`
-                        : `warning`
-                    }
-                    className="m-1"
-                  >
-                    {`v`}{tutorial.version}
-                  </Badge>
-                </div>
-              </Card.Body>
-              <Card.Footer className="text-center">
-                <Button
-                  variant="secondary"
-                  className="primary-color"
-                  href={tutorial.href}
-                  target="_blank"
-                >
-                  Try it now!
-              </Button>
-              </Card.Footer>
-            </Card>
-          </Col>
-        </a>
-      ));
-
-    const RuntimeRecipes = () => (
+    const Recipes = () => (
       <div>
         <h2>
           <translate>Runtime Recipes</translate>
@@ -193,155 +121,19 @@ class Tutorials extends React.Component {
         <hr />
         <p>
           <translate>
-            Find code samples for common patterns and best practices when
-            developing runtime modules on Substrate:
+            Code examples, common patterns, and best practices when
+            developing with Substrate:
           </translate>
         </p>
         <p>
           <Button
-            target="_blank"
             variant="secondary"
             className="primary-color"
             href="https://substrate.dev/recipes"
           >
-            <translate>Substrate Runtime Recipes ></translate>
+            <translate>Substrate Recipes ></translate>
           </Button>
         </p>
-      </div>
-    );
-
-    const RecommendedTutorials = () => (
-      <div>
-        <h2>
-          <translate>Try these first!</translate>
-        </h2>
-        <hr />
-        <Row>
-          <FeaturedTutorialCards
-            data={[
-              {
-                img: `${baseUrl}img/crates.png`,
-                title: (
-                  <translate>Create Your First Substrate Chain</translate>
-                ),
-                text: (
-                  <translate>
-                    Launch and interact with your first Substrate chain in this minimal end-to-end guide.
-                  </translate>
-                ),
-                difficulty: "easy",
-                length: "< 1",
-                prerequisite: false,
-                version: "2.0.0-rc5",
-                href: `${docUrl(
-                  "tutorials/create-your-first-substrate-chain/"
-                )}`
-              },
-              {
-                img: `${baseUrl}img/first-substrate-chain.png`,
-                title: (
-                  <translate>Build a PoE Decentralized Application</translate>
-                ),
-                text: (
-                  <translate>
-                    Build a customized Substrate chain with its own user interface.
-                  </translate>
-                ),
-                difficulty: "easy",
-                length: "1",
-                prerequisite: true,
-                version: "2.0.0-rc5",
-                href: `${docUrl(
-                  "tutorials/build-a-dapp/"
-                )}`
-              },
-              {
-                title: (
-                  <translate>Adding a Pallet to Your Runtime</translate>
-                ),
-                text: (
-                  <translate>
-                    Add the Contracts pallet or other FRAME pallets to your Substrate node template.
-                  </translate>
-                ),
-                difficulty: "medium",
-                length: "2",
-                prerequisite: false,
-                version: "2.0.0-rc4",
-                href: `${docUrl(
-                  "tutorials/add-a-pallet-to-your-runtime/")}`
-              },
-              {
-                img: `${baseUrl}img/substrate-network.png`,
-                title: (
-                  <translate>Start a Private Network with Substrate</translate>
-                ),
-                text: (
-                  <translate>
-                    Learn to start a blockchain network using an
-                    out-of-the-box Substrate node.
-                  </translate>
-                ),
-                difficulty: "easy",
-                length: "2",
-                prerequisite: false,
-                version: "2.0.0-rc5",
-                href: `${docUrl(
-                  "tutorials/start-a-private-network/"
-                )}`
-              },
-              {
-                img: `${baseUrl}img/crates.png`,
-                title: (
-                  <translate>Write a Pallet in its Own Crate</translate>
-                ),
-                text: (
-                  <translate>
-                    Make your pallets re-usable by packaging them in
-                    their own rust crate.
-                  </translate>
-                ),
-                difficulty: "medium",
-                length: "2",
-                prerequisite: false,
-                version: "2.0.0-rc5",
-                href: `${docUrl("tutorials/create-a-pallet/")}`
-              },
-              {
-                img: `${baseUrl}img/grafana.png`,
-                title: (
-                  <translate>Visualizing Node Metrics</translate>
-                ),
-                text: (
-                  <translate>
-                    Learn how to visualize the metrics that Substrate records using Prometheus.
-                  </translate>
-                ),
-                difficulty: "easy",
-                length: "< 1",
-                prerequisite: false,
-                version: "2.0.0-rc5",
-                href: `${docUrl("tutorials/visualize-node-metrics/")}`
-              },
-              {
-                img: `${baseUrl}img/ink-smart-contracts-tutorial.png`,
-                title: <translate>ink! Smart Contracts Tutorial</translate>,
-                text: (
-                  <translate>
-                    A comprehensive, end-to-end tutorial for building an ERC20
-                    token contract using ink!.
-                  </translate>
-                ),
-                difficulty: "easy",
-                length: "4",
-                prerequisite: false,
-                version: "2.0.0-rc4",
-                href:
-                  "https://substrate-developer-hub.github.io/substrate-contracts-workshop/"
-              },
-            ]}
-          />
-        </Row>
       </div>
     );
 
@@ -356,7 +148,132 @@ class Tutorials extends React.Component {
         />
         <div className="mainContainer">
           <Container>
-            <RecommendedTutorials />
+            <Row>
+              <TutorialCards
+                data={[
+                  {
+                    img: `${baseUrl}img/crates.png`,
+                    title: (
+                      <translate>Create Your First Substrate Chain</translate>
+                    ),
+                    text: (
+                      <translate>
+                        Launch and interact with your first Substrate chain in this minimal end-to-end guide.
+                      </translate>
+                    ),
+                    difficulty: "easy",
+                    length: "< 1",
+                    prerequisite: false,
+                    version: "2.0.0-rc5",
+                    href: `${docUrl(
+                      "tutorials/create-your-first-substrate-chain/"
+                    )}`
+                  },
+                  {
+                    img: `${baseUrl}img/first-substrate-chain.png`,
+                    title: (
+                      <translate>Build a PoE Decentralized Application</translate>
+                    ),
+                    text: (
+                      <translate>
+                        Build a customized Substrate chain with its own user interface.
+                      </translate>
+                    ),
+                    difficulty: "easy",
+                    length: "1",
+                    prerequisite: true,
+                    version: "2.0.0-rc5",
+                    href: `${docUrl(
+                      "tutorials/build-a-dapp/"
+                    )}`
+                  },
+                  {
+                    title: (
+                      <translate>Adding a Pallet to Your Runtime</translate>
+                    ),
+                    text: (
+                      <translate>
+                        Add the Contracts pallet or other FRAME pallets to your Substrate node template.
+                      </translate>
+                    ),
+                    difficulty: "medium",
+                    length: "2",
+                    prerequisite: false,
+                    version: "2.0.0-rc4",
+                    href: `${docUrl(
+                      "tutorials/add-a-pallet-to-your-runtime/")}`
+                  },
+                  {
+                    img: `${baseUrl}img/substrate-network.png`,
+                    title: (
+                      <translate>Start a Private Network with Substrate</translate>
+                    ),
+                    text: (
+                      <translate>
+                        Learn to start a blockchain network using an
+                        out-of-the-box Substrate node.
+                      </translate>
+                    ),
+                    difficulty: "easy",
+                    length: "2",
+                    prerequisite: false,
+                    version: "2.0.0-rc5",
+                    href: `${docUrl(
+                      "tutorials/start-a-private-network/"
+                    )}`
+                  },
+                  {
+                    img: `${baseUrl}img/crates.png`,
+                    title: (
+                      <translate>Write a Pallet in its Own Crate</translate>
+                    ),
+                    text: (
+                      <translate>
+                        Make your pallets re-usable by packaging them in
+                        their own rust crate.
+                      </translate>
+                    ),
+                    difficulty: "medium",
+                    length: "2",
+                    prerequisite: false,
+                    version: "2.0.0-rc5",
+                    href: `${docUrl("tutorials/create-a-pallet/")}`
+                  },
+                  {
+                    img: `${baseUrl}img/grafana.png`,
+                    title: (
+                      <translate>Visualizing Node Metrics</translate>
+                    ),
+                    text: (
+                      <translate>
+                        Learn how to visualize the metrics that Substrate records using Prometheus.
+                      </translate>
+                    ),
+                    difficulty: "easy",
+                    length: "< 1",
+                    prerequisite: false,
+                    version: "2.0.0-rc5",
+                    href: `${docUrl("tutorials/visualize-node-metrics/")}`
+                  },
+                  {
+                    img: `${baseUrl}img/ink-smart-contracts-tutorial.png`,
+                    title: <translate>ink! Smart Contracts Tutorial</translate>,
+                    text: (
+                      <translate>
+                        A comprehensive, end-to-end tutorial for building an ERC20
+                        token contract using ink!.
+                      </translate>
+                    ),
+                    difficulty: "easy",
+                    length: "4",
+                    prerequisite: false,
+                    version: "2.0.0-rc4",
+                    href:
+                      "https://substrate-developer-hub.github.io/substrate-contracts-workshop/"
+                  },
+                ]}
+              />
+            </Row>
           </Container>
         </div>
       </div>
