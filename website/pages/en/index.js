0da2df701ae23be7a30a35d29fcf28b7a81a766a
Dan Forbes <dan@danforbes.dev>
2020-08-26 11:41:27 -0700

    Update Homepage

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -41,26 +41,26 @@ class Index extends React.Component {
 					siteConfig={siteConfig}
 					language={language}
 					title={<translate>Substrate Developer Hub</translate>}
-					tagline={<translate>The blockchain framework that helps you focus on your vision.</translate>}
+					tagline={<translate>Blockchain Development for Innovators</translate>}
 					description={
 						<translate>
-							Substrate is a modular blockchain framework that lets you pick and choose the right
-							components for your application or enterprise.
+							Substrate is a modular framework that enables you to create purpose-built blockchains by
+							composing custom or pre-built components.
 						</translate>
 					}
 					buttonText={<translate>Get Started</translate>}
-					buttonUrl={`tutorials`}
+					buttonUrl={`docs/` + language}
 				/>
 
 				<section className='mainContainer' id='home'>
 					<Container>
 						<section className='intro-statement home-section'>
-							<span className='tagline'>Why use Substrate</span>
+							<span className='tagline'>Why Use Substrate?</span>
 
-							<h2 className='large h1'>Focus on your development.</h2>
-							<p>
-								Substrate helps you develop your blockchain fast so that you can focus on your expertise
-								while it handles the rest.
+							<h2 className='large h1'>Focus on Your Strengths</h2>
+							<p className='big'>
+								Substrate's modular design means you can reuse battle-tested libraries while building the
+								custom components that matter most.
 							</p>
 						</section>
 
@@ -70,10 +70,10 @@ class Index extends React.Component {
 									<div className='icon runtime' />
 								</section>
 								<h4>Runtime Development</h4>
-								<p>Learn how to build a custom blockchain using Substrate.</p>
+								<p>Use Substrate's FRAME runtime system to build secure, scalable blockchain logic.</p>
 								<section className='button-wrap'>
 									<a
-										href='https://substrate.dev/docs/en/tutorials/build-a-dapp/'
+										href={`docs/` + language + `/knowledgebase/runtime`}
 										className='with-arrow'>
 										Learn More
 									</a>
@@ -84,11 +84,11 @@ class Index extends React.Component {
 								<section className='icon-wrap'>
 									<div className='icon frontend' />
 								</section>
-								<h4>Front-End Development</h4>
-								<p>Learn how to build interactive user experiences with Polkadot-JS.</p>
+								<h4>Client Libraries</h4>
+								<p>Create rich user experiences for any Substrate-based chain with Polkadot-JS.</p>
 								<section className='button-wrap'>
 									<a
-										href='https://github.com/substrate-developer-hub/substrate-front-end-template'
+										href={`docs/` + language + `/knowledgebase/integrate/polkadot-js`}
 										className='with-arrow'
 									>
 										Learn More
@@ -100,8 +100,8 @@ class Index extends React.Component {
 								<section className='icon-wrap'>
 									<div className='icon smart-contract' />
 								</section>
-								<h4>Smart Contract Development</h4>
-								<p>Learn how to build Wasm smart contracts with ink!.</p>
+								<h4>Smart Contracts</h4>
+								<p>Substrate supports multiple smart contract platforms, including the EVM.</p>
 								<section className='button-wrap'>
 									<a
 										href='https://substrate.dev/substrate-contracts-workshop/#/'
@@ -118,11 +118,11 @@ class Index extends React.Component {
 							<div className='pt-5'>
 								<div className='row justify-content-center text-center py-3'>
 									<div className='col-12 col-md-8'>
-										<h4>What is Substrate</h4>
+										<span className='tagline'>What is Substrate?</span>
 										<h2 className='display-4 h1'>Everything you need to build a blockchain.</h2>
-										<p className='lead mb-4'>
-											Substrate comes with p2p networking, consensus algorithms, and cryptographic
-											libraries out-of-the-box, for free.
+										<p className='big'>
+											Substrate is powered by best-in-class cryptographic research and comes with peer-to-peer
+											networking, consensus mechanisms, and much more.
 										</p>
 									</div>
 								</div>
@@ -131,61 +131,38 @@ class Index extends React.Component {
 								<div className='col-12 col-md-4 mb-4'>
 									<div className='d-flex align-items-center mb-2'>
 										<img className='mr-2' src='/img/glyphs/rectangle-1.svg' width='40' />
-										<h3 className='mb-0'>Fast and efficient database</h3>
+										<h3 className='mb-0'>Fast and Efficient Database</h3>
 									</div>
 								</div>
 								<div className='col-12 col-md-4 mb-4'>
 									<div className='d-flex align-items-center mb-2'>
 										<img className='mr-2' src='/img/glyphs/rectangle-2.svg' width='40' />
-										<h3 className='mb-0'>Modular P2P networking stack in libp2p</h3>
+										<h3 className='mb-0'>Modular P2P Networking Stack</h3>
 									</div>
 								</div>
 								<div className='col-12 col-md-4'>
 									<div className='d-flex align-items-center mb-2'>
 										<img className='mr-2' src='/img/glyphs/rectangle-3.svg' width='40' />
-										<h3 className='mb-0'>Hot-swappable consensus layer</h3>
+										<h3 className='mb-0'>Hot-Swappable Consensus Layer</h3>
 									</div>
 								</div>
 								<div className='col-12 col-md-4 mb-4'>
 									<div className='d-flex align-items-center mb-2'>
 										<img className='mr-2' src='/img/glyphs/rectangle-4.svg' width='40' />
-										<h3 className='mb-0'>Customizable transaction queue management system</h3>
+										<h3 className='mb-0'>Configurable Transaction Queue</h3>
 									</div>
 								</div>
 								<div className='col-12 col-md-4 mb-4'>
 									<div className='d-flex align-items-center mb-2'>
 										<img className='mr-2' src='/img/glyphs/rectangle-5.svg' width='40' />
-										<h3 className='mb-0'>Rich framework for composing runtime logic.</h3>
+										<h3 className='mb-0'>Flexible Runtime Library</h3>
 									</div>
 								</div>
 								<div className='col-12 col-md-4'>
 									<div className='d-flex align-items-center mb-2'>
 										<img className='mr-2' src='/img/glyphs/rectangle-6.svg' width='40' />
-										<h3 className='mb-0'>Mobile light client support</h3>
+										<h3 className='mb-0'>Light Client Optimized</h3>
 									</div>
-								</div>
-							</div>
-						</div>
-					</section>
-
-					<section className='bg-light call-outs first'>
-						<div className='container'>
-							<div className='row justify-content-between align-items-center py-5'>
-								<div className='col-12 col-md-5 order-2 order-md-1'>
-									<h2 className='h1'>Smart contract ready</h2>
-									<p className='large mb-4'>
-										Substrate has a Wasm smart contract platform that you can use out of the box.
-										Because Substrate uses Wasm, you can build your smart contracts using any
-										compatible language. We have built ink!, a Rust-based eDSL for this purpose.
-									</p>
-									<a
-										className='action-link'
-										href='https://substrate.dev/docs/en/knowledgebase/smart-contracts/overview'>
-										<span>Learn more about ink!</span>
-									</a>
-								</div>
-								<div className='col-12 col-md-6 order-1 order-md-2 pl-md-0 mb-4 mb-md-0 text-center'>
-									<img className='w-25' src='/img/pictures/substrate-wasm.svg' />
 								</div>
 							</div>
 						</div>
@@ -202,12 +179,12 @@ class Index extends React.Component {
 									/>
 								</div>
 								<div className='col-12 col-md-5'>
-									<h2 className='h1'>(Almost) production ready</h2>
+									<h2 className='h1'>Production Ready</h2>
 									<p className='large mb-4'>
 										Substrate is the backbone that powers Polkadot, a next generation,
-										heterogeneous, multi-chain network. Most 'parachains' that will connect to this
-										network are also built on Substrate. Substrate is undergoing a security audit in
-										preparation for a 2020 release of the Polkadot network.
+										heterogeneous, multi-chain network. Most of the blockchains in the Polkadot
+										ecosystem are also built on Substrate. The Polkadot Network was launched in
+										May of 2020.
 									</p>
 									<a className='action-link' href='https://polkadot.network/technology/'>
 										<span>Learn more about Polkadot</span>
@@ -217,12 +194,35 @@ class Index extends React.Component {
 						</div>
 					</section>
 
+					<section className='bg-light call-outs first'>
+						<div className='container'>
+							<div className='row justify-content-between align-items-center py-5'>
+								<div className='col-12 col-md-5 order-2 order-md-1'>
+									<h2 className='h1'>Smart Contract Ready</h2>
+									<p className='large mb-4'>
+										Substrate has a Wasm smart contract platform that you can use out of the box.
+										Because Substrate uses Wasm, you can build your smart contracts using any
+										compatible language. We have built ink!, a Rust-based eDSL, for this purpose.
+									</p>
+									<a
+										className='action-link'
+										href='https://substrate.dev/docs/en/knowledgebase/smart-contracts/overview'>
+										<span>Learn more about ink!</span>
+									</a>
+								</div>
+								<div className='col-12 col-md-6 order-1 order-md-2 pl-md-0 mb-4 mb-md-0 text-center'>
+									<img className='w-25' src='/img/pictures/substrate-wasm.svg' />
+								</div>
+							</div>
+						</div>
+					</section>
+
 					<section className='bg-white learn'>
 						<div className='container'>
 							<div className='row justify-content-center text-center pt-5'>
 								<div className='col-12 col-md-10'>
-									<h4>Learning Substrate</h4>
-									<h2 className='h1'>More ways to learn Substrate.</h2>
+									<span className='tagline'>Keep Exploring!</span>
+									<h2 className='display-4 h1'>There are lots of ways to learn about Substrate.</h2>
 								</div>
 							</div>
 						</div>
@@ -237,13 +237,12 @@ class Index extends React.Component {
 											<div className='py-5 py-md-0'>
 												<h2>Substrate Seminar</h2>
 												<p className='mb-3'>
-													Substrate Seminar is an open collaborative learning call where we
-													learn about Substrate together.
+													An open, collaborative group for learning about Substrate and connecting with other builders.
 												</p>
 												<a
 													className='btn primary-color'
 													href='https://substrate.dev/en/seminar'>
-													Join the learning group
+													Join the Learning Group
 												</a>
 											</div>
 											<div className='border-bottom d-md-none' />
@@ -255,15 +254,14 @@ class Index extends React.Component {
 									<div className='row justify-content-center'>
 										<div className='col-12 col-lg-8'>
 											<div className='py-5 py-md-0'>
-												<h2>Substrate Playground</h2>
+												<h2>Substrate Recipes</h2>
 												<p className='mb-3'>
-													Start hacking your substrate runtime in a web based VSCode like IDE,
-													with full terminal support.
+													A collection of working code examples that solve common problems.
 												</p>
 												<a
 													className='btn primary-color'
-													href='https://playground.substrate.dev'>
-													Start playing in your browser
+													href='https://substrate.dev/recipes'>
+													Browse the Recipes
 												</a>
 											</div>
 										</div>
@@ -282,9 +280,7 @@ class Index extends React.Component {
 									</div>
 									<div className='col-12 col-md-8'>
 										<p className='lead mb-4'>
-											Get started with the documentation or drop in the Riot chat to ask technical
-											questions, meet others who share your interests, or keep an eye on what's
-											going on
+											Get started with our helpful documentation or ask questions in our technical chat!
 										</p>
 										<div className='d-flex justify-content-center'>
 											<div className='px-1'>
@@ -297,7 +293,7 @@ class Index extends React.Component {
 											<div className='px-1'>
 												<a
 													className='btn btn-lg btn-outline-primary'
-													href='https://riot.im/app/#/room/!HzySYSaIhtyWrwiwEV:matrix.org'>
+													href='https://app.element.io/#/room/!HzySYSaIhtyWrwiwEV:matrix.org'>
 													Ask Questions
 												</a>
 											</div>
