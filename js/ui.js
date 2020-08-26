67d179cc4435e670d231f066dd277f8d6a3b536a
Devhub Deployer <devhub-deploy@users.noreply.github.com>
2020-08-26 18:24:32 +0000

    Deploy website
    Deploy website version based on 9986eeb1de19a0e8c4efb73499b834d643a47ed7

---
+++ js/ui.js
@@ -0,0 +1,2 @@
+var a_recipe = document.querySelectorAll(".nav-site a[href='/recipes']")[0].setAttribute('target', '_blank')
+var a_git = document.querySelectorAll(".nav-site a[href='https://github.com/paritytech/substrate']")[0].setAttribute('target', '_blank')
