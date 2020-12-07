3fea3789877b593159593a45cc03b3fad73b4a2a
Imad Arain <arain.imad@gmail.com>
2020-12-07 08:08:36 +0100

    Commenting out the announcement bar for Polkadot Decoded

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -32,9 +32,9 @@ class Index extends React.Component {
 
 		return (
 			<section>
-				<div className="announcement">
+				{/* <div className="announcement">
 					The first Polkadot community conference is coming Dec 3rd!<br/> <a href="https://parity.link/UFsVd">Register Here</a> for Polkadot Decoded.
-				</div>
+				</div> */}
 				<HomeSplash
 					id='home-hero'
 					siteConfig={siteConfig}
