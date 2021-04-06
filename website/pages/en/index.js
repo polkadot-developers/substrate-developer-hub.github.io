43161c012bbc72635578fc11137598e57e7d91fc
Imad Arain <arain.imad@gmail.com>
2021-04-06 18:31:06 +0200

    disabling Polkadot Decoded Vote ad

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -36,11 +36,11 @@ class Index extends React.Component {
 
     return (
       <section>
-        <div className="announcement">
+        {/* <div className="announcement">
           Polkadot Decoded 2021 - The vote is on.<br/> 
           Vote for your favorite talks and help curate the final conference program.<br/>
           <a href="https://decoded.polkadot.network/?utm_source=substrate.dev&utm_medium=referral&utm_campaign=decoded2&utm_content=notification+bar+voting">Vote now!</a>
-        </div>
+        </div> */}
         <HomeSplash
           id='home-hero'
           siteConfig={siteConfig}
